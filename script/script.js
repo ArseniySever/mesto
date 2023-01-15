71ac9735e728b6db8fc751b396cdd8352c2b5dec
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-01-15 10:54:03 +0300

    fix: внесены правки после код-ревью :D

--- script/script.js
+++ script/script.js
@@ -6,14 +6,18 @@ let formElement = page.querySelector('.popup__form');
 let login = page.querySelector('.profile__title');
 let job = page.querySelector('.profile__subtitle');
 let nameInput = formElement.querySelector('.popup__name-item_value_name');
-let jobInput = formElement.querySelector('.popup__name-item_value_name');
-let save = page.querySelector('.popup__save');
+let jobInput = formElement.querySelector('.popup__name-item_value_job');
+let save = popup.querySelector('.popup__save');
 let heart = document.querySelector('.element__heart');
 
-function open_popup() {
+function openPopup() {
+    nameInput.value = login.textContent;
+    jobInput.value = job.textContent;
     popup.classList.add('popup_opened');
+
+
 }
-function close_popup() {
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
@@ -21,16 +25,17 @@ function handleFormSubmit(evt) {
     evt.preventDefault();
     login.textContent = nameInput.value;
     job.textContent = jobInput.value;
+    save.addEventListener('click', closePopup);
+
 }
 
-function click_heart() {
+function clickHeart() {
     heart.classList.add('element__heart_active');
 }
 
-heart.addEventListener('click', click_heart);
-editButton.addEventListener('click', open_popup);
+heart.addEventListener('click', clickHeart);
+editButton.addEventListener('click', openPopup);
 formElement.addEventListener('submit', handleFormSubmit);
-save.addEventListener('click', close_popup);
-closeButton.addEventListener('click', close_popup);
+closeButton.addEventListener('click', closePopup);
 
 
