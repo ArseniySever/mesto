d2531bf04f0fa34a9795dc9260facc94b746fb6d
Arseniy Potashov <potashov-arseniy@yandex.ru>
2023-02-02 13:08:32 +0300

    fix: исправление ошибок после ревью

---
+++ script/card.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Тихвин',
+        link: 'https://sun6-23.userapi.com/s/v1/if1/tfLbly28AUsC2zFfiGLw_DGpr8qTPjXO4ko6czg5B9TWl_Cs1chlVb_6gFmKr6aUQJvDV5fY.jpg?size=902x902&quality=96&crop=170,0,902,902&ava=1'
+    },
+    {
+        name: 'Старая Ладога',
+        link: 'https://35photo.pro/photos_main/1266/6330385.jpg'
+    },
+    {
+        name: 'Гатчина',
+        link: 'https://sun9-61.userapi.com/tUYFzmhM9m6W-cY17yM7qcT2_XEupql_ml-ORQ/4UBZEAvWm7Q.jpg'
+    },
+    {
+        name: 'Выборг',
+        link: 'https://kareliya-tur.ru/images/foto-vyborg/zimoj/vyborgskij-zamok-zimoj.jpg'
+    },
+    {
+        name: 'Петергорф',
+        link: 'http://news-piter.ru/wp-content/uploads/2012/03/DSC_9151.jpg'
+    },
+    {
+        name: 'Орешек',
+        link: 'https://takeyourtrip.ru/images/posts/8789/HQ/IMG_5287.jpg'
+    }
+];
--- script/script.js
+++ script/script.js
@@ -1,146 +1,112 @@
-var page = document.querySelector('.page');
-var popup = document.querySelectorAll('.popup');
-var popupProfile = page.querySelector('.popup-profile');
-var editButton = page.querySelector('.profile__edit-button');
-var popupPlace = page.querySelector('.popup-place');
-var popupImage = page.querySelector('.popup-image');
+let page = document.querySelector('.page');
+let popupList = document.querySelectorAll('.popup');
+let popupProfile = page.querySelector('.popup-profile');
+let editButton = page.querySelector('.profile__edit-button');
+let popupPlace = page.querySelector('.popup-place');
+let popupImage = page.querySelector('.popup-image');
 
-var addButton = page.querySelector('.profile__add-button');
+let addButton = page.querySelector('.profile__add-button');
 
-var closeButton = document.querySelectorAll('.popup__close');
-var formElementProf = page.querySelector('.popup__form-profile');
-var formElementPla = page.querySelector('.popup__form-place');
+let closeButtonList = document.querySelectorAll('.popup__close');
+let formElementProfile = page.querySelector('.popup__form-profile');
+let formElementPlace = page.querySelector('.popup__form-place');
 
-var login = page.querySelector('.profile__title');
-var job = page.querySelector('.profile__subtitle');
-var nameInput = formElementProf.querySelector('.popup__name-item_value_name');
-var jobInput = formElementProf.querySelector('.popup__name-item_value_job');
-let titleInput = formElementPla.querySelector('.popup__name-item_value_title');
-var imageInput = formElementPla.querySelector('.popup__name-item_value_link');
+let login = page.querySelector('.profile__title');
+let job = page.querySelector('.profile__subtitle');
+let nameInput = formElementProfile.querySelector('.popup__name-item_value_name');
+let jobInput = formElementProfile.querySelector('.popup__name-item_value_job');
+let titleInput = formElementPlace.querySelector('.popup__name-item_value_title');
+let imageInput = formElementPlace.querySelector('.popup__name-item_value_link');
 const placesBox = document.querySelector(".elements");
 const placeTemplate = document.querySelector("#element").content;
-var trash = document.querySelectorAll('.element__trash');
-var heart = document.querySelectorAll('.element__heart');
-
 const creatButton = document.querySelector(".creat__button");
 
-const initialCards = [
-    {
-        name: 'Тихвин',
-        link: 'https://sun6-23.userapi.com/s/v1/if1/tfLbly28AUsC2zFfiGLw_DGpr8qTPjXO4ko6czg5B9TWl_Cs1chlVb_6gFmKr6aUQJvDV5fY.jpg?size=902x902&quality=96&crop=170,0,902,902&ava=1'
-    },
-    {
-        name: 'Старая Ладога',
-        link: 'https://35photo.pro/photos_main/1266/6330385.jpg'
-    },
-    {
-        name: 'Гатчина',
-        link: 'https://kartinkin.net/pics/uploads/posts/2022-08/1659628366_24-kartinkin-net-p-osennii-peterburg-priroda-krasivo-foto-24.jpg'
-    },
-    {
-        name: 'Выборг',
-        link: 'https://kareliya-tur.ru/images/foto-vyborg/zimoj/vyborgskij-zamok-zimoj.jpg'
-    },
-    {
-        name: 'Петергорф',
-        link: 'http://news-piter.ru/wp-content/uploads/2012/03/DSC_9151.jpg'
-    },
-    {
-        name: 'Орешек',
-        link: 'https://takeyourtrip.ru/images/posts/8789/HQ/IMG_5287.jpg'
-    }
-];
 
-const placeInf = initialCards.map(function (item) {
-    return {
-        name: item.name,
-        link: item.link
-    };
-});
+function addCard(name, link) {
+    const placeElement = document.querySelector("#element").content; 
+    const newElement = placeElement.querySelector('.element__rectangle').cloneNode(true);  
+    newElement.querySelector(".element__title").textContent = name;
+    newElement.querySelector(".element__image").src = link;
+    newElement.querySelector(".element__image").alt = name;
+    newElement
+        .querySelector('.element__heart')
+        .addEventListener('click', likeButton);
+        newElement
+        .querySelector('.element__trash')
+        .addEventListener('click', DeleteOnClick);
+    newElement.querySelector('.element__image').addEventListener('click', function() {
+        popupImage.querySelector(".popup__image").src = link;
+        popupImage.querySelector(".popup__subtitle").textContent = name;
+        openPopup(popupImage);
+    });
+    return newElement;
+}
+
+
+function arrayInitialCards(initialCards) {
+    initialCards.forEach((item) => {
+        placesBox.append(addCard(item.name, item.link));
+    })
+}
+
 function closePopup() {
-    for (var i = 0; i < popup.length; i++) {
-            popup[i].classList.remove('popup_opened');
+    for (var i = 0; i < popupList.length; i++) {
+            popupList[i].classList.remove('popup_opened');
         };
 }
 
-function lovePlace(e) {
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+function likeButton(e) {
+    
     e.target.classList.toggle("element__heart_active");
 }
-function creation() {
-    placeInf.forEach(creationCart);
-}
 
-function onClickDelite(e) {
+
+function DeleteOnClick(e) {
     e.target.closest(".element__rectangle").remove();
 }
-function creationCart({ name, link }) {
-    
-    const placeElement = placeTemplate.querySelector(".element__rectangle").cloneNode(true);
-    placeElement.querySelector(".element__title").textContent = name;
-    placeElement.querySelector(".element__image").src = link;
-    
-    placeElement.querySelector('.element__image').addEventListener ("click", ()=> {
-        popupImage.classList.add('popup_opened');
-        popupImage.querySelector(".popup__image").src = link;
-        popupImage.querySelector(".popup__subtitle").textContent = name;
-       
-    });
-    placeElement
-        .querySelector('.element__heart')
-        .addEventListener('click', lovePlace);
-    placeElement
-        .querySelector('.element__trash')
-        .addEventListener('click', onClickDelite);
-    placesBox.prepend(placeElement);
-    return placeElement;
-    
 
-}
 
 function openPopupProfile() {
     nameInput.value = login.textContent;
     jobInput.value = job.textContent;
-    popupProfile.classList.add('popup_opened');
+    openPopup(popupProfile);
     
 }
 function openPopupEdit() {
-    popupPlace.classList.add('popup_opened');
-    
+    openPopup(popupPlace);    
 }
 
 
-function handleFormSubmit(evt) {
+function handleFormSubmitProfile(evt) {
     evt.preventDefault();
     login.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    closePopup();
+    closePopup(popupProfile);
 
 }
 
-function creatPlace(evt){
-    evt.preventDefault();
-    const placeElement = placeTemplate.querySelector(".element__rectangle").cloneNode(true);
-    placeElement. querySelector('.element__title').textContent = titleInput.value;
-    placeElement. querySelector('.element__image').src = imageInput.value;
-    closePopup();
-    placeElement
-        .querySelector('.element__heart')
-        .addEventListener('click', lovePlace);
-    placeElement
-        .querySelector('.element__trash')
-        .addEventListener('click', onClickDelite);
-    placesBox.prepend(placeElement);
-    return placeElement;
+
+
+function creatPlace(event) {
+    event.preventDefault();
+    placesBox.prepend(addCard(titleInput.value, imageInput.value));
+
+    closePopup(popupPlace);
+    event.target.reset(); 
 }
 
-creation();
 addButton.addEventListener('click', openPopupEdit);
 editButton.addEventListener('click', openPopupProfile);
-closeButton.forEach(button => button.addEventListener("click", closePopup));
-formElementProf.addEventListener('submit', handleFormSubmit);
-formElementPla.addEventListener('submit', creatPlace);
-trash.forEach(button => button.addEventListener("click", onClickDelite));
-heart.forEach(button => button.addEventListener("click", lovePlace));
+closeButtonList.forEach(button => button.addEventListener("click", closePopup));
+formElementProfile.addEventListener('submit', handleFormSubmitProfile);
+formElementPlace.addEventListener('submit', creatPlace);
+
+arrayInitialCards(initialCards);
+
 
 
 
